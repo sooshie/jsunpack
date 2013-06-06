dc7cf23c892dac06635812056b8fdec1fd801b38
David Dorsey <dorsey@visiblerisk.com>
2013-06-05 23:05:14 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup (name = 'jsunpack',
-    version = '1.0',
+    version = '1.05',
     author      = "David Dorsey",
     description = 'A generic javascript unpacker',
     packages=['jsunpack']
